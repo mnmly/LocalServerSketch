7d21ebbe5899cf7116da2f3f229bd00672e1af14
HIROAKI YAMANE <i.am+mac@mnmly.com>
2021-03-15 09:56:29 +0000

    Initial Commit

---
+++ LocalServerApp/LocalServerAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  LocalServerAppApp.swift
+//  LocalServerApp
+//
+//  Created by HIROAKI YAMANE on 15/03/2021.
+//
+
+import SwiftUI
+
+@main
+struct LocalServerAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
